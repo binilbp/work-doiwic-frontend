b4e3ac6f3c425c2e0bec96f44280c10c546b82bb
binilbp <biniloffcl@gmail.com>
2026-05-23 17:42:35 +0000

    Delete redundant code

--- src/lib/helpers.svelte.js
+++ src/lib/helpers.svelte.js
@@ -41,35 +41,18 @@ export function previousStep() {
 export async function fetchAndSummarize(endpoint, userInput) {
     if (!userInput.trim()) return null;
 
-    if (endpoint === "role") {
-        const response = await fetch(`http://localhost:8000/profile`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ input: userInput.trim() })
-        });
+    const response = await fetch(`http://localhost:8000/${endpoint}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ input: userInput.trim(), user_info: appState.user_context })
+    });
 
-        if (!response.ok) {
-            throw new Error("A network or server error occurred.");
-        }
-
-        console.log("Recieved response");
-        return await response.json(); 
+    if (!response.ok) {
+        throw new Error("A network or server error occurred.");
     }
 
-    else {
-        const response = await fetch(`http://localhost:8000/${endpoint}`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ input: userInput.trim(), user_info: appState.user_context })
-        });
-
-        if (!response.ok) {
-            throw new Error("A network or server error occurred.");
-        }
-
-        console.log("Recieved response");
-        return await response.json(); 
-    }
+    console.log("Recieved response");
+    return await response.json(); 
 
 }
 
